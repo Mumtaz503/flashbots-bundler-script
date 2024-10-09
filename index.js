6a47b7cb2192fb95675bd884d06fb3bc17c02eea
mumtaz503 <hasaanahmad059@gmail.com>
2024-10-09 17:15:40 +0500

    cleared some errors regarding transactions sorting

--- index.js
+++ index.js
@@ -7,9 +7,20 @@ require("dotenv").config();
 
 const provider = new ethers.JsonRpcProvider(process.env.SEPOLIA_RPC_URL);
 const abiCoder = AbiCoder.defaultAbiCoder();
-const privateKeys = [process.env.PRIVATE_KEY_1, process.env.PRIVATE_KEY_2];
+const privateKeys = [
+  process.env.PRIVATE_KEY_1,
+  process.env.PRIVATE_KEY_2,
+  process.env.PRIVATE_KEY_3,
+  process.env.PRIVATE_KEY_4,
+  process.env.PRIVATE_KEY_5,
+  // process.env.PRIVATE_KEY_6,
+  // process.env.PRIVATE_KEY_7,
+  // process.env.PRIVATE_KEY_8,
+  // process.env.PRIVATE_KEY_9,
+  // process.env.PRIVATE_KEY_10,
+];
 
-const TOKEN = "0x2317459F4c2d80bbeb5127d3d91ed3dCd09b42f0";
+const TOKEN = "0x2ec75aD34Aa58C1415C602407ef0E9aFD6f22021";
 const TOKEN_ABI = ["event TradingOpen(bool tradingOpen_)"];
 const WETH = "0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9";
 const UNISWAP_ROUTER_ADDRESS = "0xC532a74256D3Db42D0Bf7a0400fEFDbad7694008";
@@ -32,14 +43,18 @@ const startTransmission = async () => {
   );
 
   const GWEI = BigInt(10 ** 10);
-  const LEGACY_GAS_PRICE = GWEI * 30n;
-  const PRIORITY_FEE = GWEI * 70n;
+  const PRIORITY_FEE = GWEI * 50n;
   const blockNumber = await provider.getBlockNumber();
   const block = await provider.getBlock();
   const maxBaseFeeInFutureBlock =
     FlashbotsBundleProvider.getMaxBaseFeeInFutureBlock(block.baseFeePerGas, 6);
 
   console.log(`Max fee in future Block: ${String(maxBaseFeeInFutureBlock)}`);
+
+  const blockGasLimit = block.gasLimit;
+  console.log(`Block Gas Limit: ${blockGasLimit.toString()}`);
+
+  let totalGasUsed = BigInt(0);
 
   const transactions = signers.map((signer) => {
     const data = abiCoder.encode(
@@ -60,13 +75,25 @@ const startTransmission = async () => {
       data: txData,
       chainId: 11155111,
       gasLimit: 500000,
-      value: ethers.parseEther("0.00005"),
+      value: ethers.parseEther("0.01"),
     };
+
+    totalGasUsed += BigInt(swapTransaction.gasLimit);
+
     return {
       signer: signer,
       transaction: swapTransaction,
     };
   });
+
+  console.log(`Total Gas Used by Bundle: ${totalGasUsed.toString()}`);
+
+  if (totalGasUsed > blockGasLimit) {
+    console.error(
+      `Total gas usage exceeds the block's gas limit. Consider splitting the bundle.`
+    );
+    process.exit(1);
+  }
 
   const signedTransactions = await flashbotsProvider.signBundle(transactions);
 
